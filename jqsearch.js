d9407a0e73fde50b90630398bac53ebe303d258d
Alex Funaro <afunaro@gmail.com>
2014-02-22 20:56:14 -0500

    Working simple prototype

--- jqsearch.js
+++ jqsearch.js
@@ -23,31 +23,40 @@ JQuery Search created by Alex Funaro @A_funs
             search_class: "jq_searchable",
             item_holder_class: "jq_item_holder",
             highlight_class:"highlight",
-            keystroke:true
+            keystroke:true,
+            return_location:'taker'
         }, options );
         
         var itemArray = [];
-
         //push all search items to array
-        $('.+settings.item_holder_class+').each(function(el){
+        $('.'+settings.item_holder_class).each(function(ind, el){
           itemArray.push(el);
         })
         
         $(this).on({
-          change: doSearch(this),
-          keyup: doSearch(this),
-          keydown: doSearch(this)
+          change: function(){doSearch(this)},
+          keyup: function(){doSearch(this)},
+          keydown: function(){doSearch(this)}
         })
-        
-        function doSearch(this){
-          $(this).val();
+        function doSearch(tosearch){
+          var searchedRaw = $(tosearch).val();
+          //find search results
+          var results = [];
+          $(itemArray).each(function(ind,item){
+            var fields_to_search = $(item).find('.'+settings.search_class);
+              $(fields_to_search).each(function(ind, element){
+                var re = new RegExp(searchedRaw, 'g');
+                if($(element).text().match(re)){
+                  results.push(item);  
+                }                
+              })
+          })
+        // reset and return 
+          $("."+settings.return_location).html('');
+          $(results).each(function(ind, el){
+            $(el).appendTo('.'+settings.return_location);
+          })
         }
-        
-        // Greenify the collection based on the settings variable.
-        return this.css({
-            color: settings.color,
-            backgroundColor: settings.backgroundColor
-        });
  
     };
  
